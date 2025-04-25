1d41b0e41dcd157a235c4c20fff85fc6fa553883
amirah <mirez.data@gmail.com>
2025-04-25 18:24:07 +0800

    commit changes

--- views/partials/header.php
+++ views/partials/header.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html class="h-full bg-gray-100">
+<html class="">
   <head >
     <meta charset="UTF-8">
     <title>Simple GuestBook</title>
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" 
     rel="stylesheet"><link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
   </head>
-  <body class="h-full">
-    <div class="min-h-full">
+  <body class="">
+    <div class="">
