6067092c98ba8929f2c5512f4db6b8dd04337799
amirah <mirez.data@gmail.com>
2025-04-25 23:22:38 +0800

    testing the database

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php 
 
 require 'views/index.view.php';
+require 'Database.php';
+
+$db = new Database();
+$entry = $db->query('SELECT * FROM entry');
+
+foreach ($entry as $row){
+  echo "<li>".$row['name']."</li>";
+  echo "<li>".$row['message']."</li>";
+}
